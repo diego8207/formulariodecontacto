6ed6fe438033ad1499b24321350040fb419fa5f6
Diego Armando Velez Peralta <si.macrosystem@gmail.com>
2017-02-07 12:00:02 -0500

    Trabajo Confirmado.

--- app/src/main/java/co/macrosystem/formulariodecontacto/MainActivity.java
+++ app/src/main/java/co/macrosystem/formulariodecontacto/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                     updateDisplay();
                 }
             };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
